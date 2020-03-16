849b7d968f7418716c3523a2adc25b822ae8c4d4
s6beotto <s6beotto@uni-bonn.de>
2020-03-16 09:29:27 +0100

    Added function getOutputFilename

--- src/tools.py
+++ src/tools.py
@@ -16,6 +16,15 @@ libmetropolis.metropolis.restype = ctypes.POINTER(ctypes.c_double)
 libmetropolis.get_accept_ratio.argtypes = ()
 libmetropolis.get_accept_ratio.restype = ctypes.c_double
 
+def getOutputFilename(relative_path: Path, suffix: str, args_output = None, subdir: str = 'imgs', filetype: str = 'pdf') -> Path:
+	if args_output != None:
+		return args_output
+	out_filename = getRootDirectory() / subdir / relative_path
+	out_filename.parent.mkdir(parents=True, exist_ok=True)
+
+	out_filename = out_filename.with_suffix('')
+	return Path('%s_%s.%s' %(out_filename, suffix, filetype))
+
 def getMinima(lambda_):
 	# mimina at +/- sqrt(-a/(2b))
 	return np.sqrt(-1 / (2 * lambda_))
