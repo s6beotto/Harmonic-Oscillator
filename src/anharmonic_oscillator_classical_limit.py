Fixed bug leading to wrong sign of mu in *.cfg files

src/anharmonic_oscillator_classical_limit.py
@@ -18,7 +18,7 @@ parser.add_argument('-N', '--number', type=int, default=100,
                     help='Number of lattice sites')
 parser.add_argument('-m', '--mass', type=float, default=0.01,
                     help='Mass of the particle')
-parser.add_argument('-u', '--mu', type=float, default=10,
+parser.add_argument('-u', '--mu', type=float, default=-10,
                     help='Depth of the potential')
 parser.add_argument('-t', '--tau', type=float, default=0.1,
                     help='Time step size')
@@ -80,7 +80,7 @@ bins = np.arange(bins_min, bins_max + bins_step, bins_step)
 
 def calculatePositionDistribution(hbar):
 	print('calculating for hbar=%0.4f' % hbar)
-	m = Metropolis(init=initial, valWidth=1, initValWidth=initial_random, hbar=hbar, tau=tau, N=N, m=mass, lambda_=lambda_, mu=-mu)
+	m = Metropolis(init=initial, valWidth=1, initValWidth=initial_random, hbar=hbar, tau=tau, N=N, m=mass, lambda_=lambda_, mu=mu)
 
 	vals = next(islice(m, iterations, iterations + 1))			# get iterations th metropolis iteration
 	return list(np.histogram(vals[0], bins)[0]), vals[1]