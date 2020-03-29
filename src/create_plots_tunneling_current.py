054c3a467143f922613eb732fc5cfdde6d70add1
s6beotto <s6beotto@uni-bonn.de>
2020-03-29 17:08:01 +0200

    Added option to fit an exp function to the data

--- src/create_plots_tunneling_current.py
+++ src/create_plots_tunneling_current.py
@@ -2,7 +2,8 @@
 
 # import modules
 from matplotlib import pyplot as plt
-from tools import getRootDirectory, getOutputFilename
+import scipy.optimize as op
+from tools import getRootDirectory, getOutputFilename, getColorIterator
 import csv
 import numpy as np
 
@@ -14,6 +15,7 @@ import configparser
 parser = argparse.ArgumentParser(description='Plot the energy depending on the metropolis sample.')
 parser.add_argument('filename', type=pathlib.Path, help='Input filename')
 parser.add_argument('-l', '--log', action='store_true')
+parser.add_argument('-f', '--fit', type=float, default=None, help='Fit an exponential function to the data, starting with the given value')
 parser.add_argument('-o', '--output', type=pathlib.Path, help='Output filename')
 args = parser.parse_args()
 
@@ -56,11 +58,31 @@ N = config['DEFAULT'].getfloat('n', fallback=100)
 # plot
 plt.figure()
 
-plt.errorbar(distances, transitions / N, yerr = dtransitions / N, fmt='.')
+color_iterator = getColorIterator()
+color_plot, color_fit = next(color_iterator)['color']
+
+plt.errorbar(distances, transitions / N, yerr = dtransitions / N, fmt='.', color=color_plot)
 plt.xlabel('Distance')
 plt.ylabel('Tunneling rate')
 if args.log:
 	plt.yscale('log')
+
+if args.fit:
+	def exp_decay(x, *p):
+		A, c = p
+		return A * np.exp(-x / c)
+
+	initvals = [1, 1]
+	filter_ = (distances > args.fit) * (transitions > 5)
+	parameters, parameters_error = op.curve_fit(exp_decay, distances[filter_], transitions[filter_] / N, p0=initvals, sigma=dtransitions[filter_] / N)
+
+	parameters_error = np.sqrt(np.diag(parameters_error))
+
+	xdata_fit = np.linspace(min(distances), max(distances), 1000)
+	ydata_fit = exp_decay(xdata_fit, *parameters)
+
+	plt.plot(xdata_fit, ydata_fit, scaley=False, label='$f(x) = A \cdot \exp(-x / b)$\n$A = (%.0f \pm %.0f)$\n$b = (%.2f \pm %.2f)$' %(parameters[0], parameters_error[0], parameters[1], parameters_error[1]), color=color_fit)
+	plt.legend()
 
 
 # filesystem stuff
